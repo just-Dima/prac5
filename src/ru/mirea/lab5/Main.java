e7579e0930d141479931beba70d3f1f1ea57c3a6
Dima <kakdimaaa@mail.ru>
2020-10-19 01:43:14 +0300

    Initial commit

---
+++ src/ru/mirea/lab5/Main.java
@@ -0,0 +1,17 @@
+package ru.mirea.lab5;
+import java.util.Scanner;
+public class Main {
+    public static String recursion(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        //System.out.println(n);
+
+        return recursion(n - 1) + " " + n;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(recursion(10));
+    }
+}
